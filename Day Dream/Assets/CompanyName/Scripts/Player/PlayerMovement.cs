bfe38f3470134b09009b3cd04b05d7ee6e5c6a3d
theRealTinyRick <31013885+theRealTinyRick@users.noreply.github.com>
2018-09-08 20:22:44 -0400

    Added Tools and Odin Set Up
    The next commit should have rewired setup and a new animator controller

---
+++ Day Dream/Assets/CompanyName/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    [TabGroup("Preferences")]
+    [SerializeField] 
+    private float timeTillLand;
+    
+    [TabGroup("Preferences")]
+    [SerializeField] 
+    private float timeTillRoll;
+    
+    [TabGroup("Set Up")]
+    [SerializeField]
+    private PlayerTargeting pTargeting;
+
+    private float fallMultiplyer = 10f;
+    private float lowJumpMultiplyer = 3f;
+    private float timeSinceJump = 0.0f;
+    
+    private Rigidbody rb;
+    private Animator anim;
+    private GameObject pCamera;
+    private PlayerManager pManager;
+    private PlayerController pController;
+
+    private void Start(){
+        pManager = PlayerManager.instance;
+        pController = GetComponent<PlayerController>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        pCamera = Camera.main.gameObject;
+    }
+
+    private void FixedUpdate(){
+        BetterJumpPhysics();
+    }
+
+    public void FreeMovement(Vector3 movement, float speed){
+        if (PlayerManager.currentState != PlayerManager.PlayerState.Attacking){
+            if(pManager.isLockedOn && pManager.isBlocking){
+                speed = speed/2.5f;
+            }else if(!pController.CheckGrounded()){
+                // speed = speed / 1.5f;
+            }
+
+            movement = pCamera.transform.TransformDirection(movement);
+            movement.y = 0;
+            movement = Vector3.Normalize(movement);
+
+            rb.velocity = new Vector3(movement.x * speed, rb.velocity.y, movement.z * speed);
+
+            if(pController.CheckGrounded() && movement != Vector3.zero){
+                if(!pManager.isLockedOn || !pManager.isBlocking){
+                    float turnSpeed = Mathf.Max(Mathf.Abs(movement.x), Mathf.Abs(movement.z));
+                    if(turnSpeed > .5f){
+                        turnSpeed = 0.5f;
+                    }
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), turnSpeed);
+                }
+            }
+        }
+    }
+
+    public void AnimatePlayerWalking(Vector3 moveDir){
+        if(pManager.isLockedOn && pManager.isBlocking){
+            if(moveDir.z > 0.5f){
+                moveDir.z = 0.5f;
+            }
+            anim.SetFloat("velocityY", Mathf.Lerp(anim.GetFloat("velocityY"), moveDir.z, .1f));
+            anim.SetFloat("velocityX", Mathf.Lerp(anim.GetFloat("velocityX"), moveDir.x, .1f));
+            LookAtTarget(pTargeting.currentTarget.transform);
+        }else{
+            float val = Mathf.Lerp(anim.GetFloat("velocityY"), Mathf.Max(Mathf.Abs(moveDir.x), Mathf.Abs(moveDir.z)), 0.3f);
+            anim.SetFloat("velocityY", val);
+        }
+    }
+
+    public void LookAtTarget(Transform target){
+        Vector3 tp = target.position;
+        tp.y = transform.position.y;
+        Quaternion rot = Quaternion.LookRotation(tp - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, .2f);
+    }
+
+    public void Jump(float jumpHeight, Vector3 dir = new Vector3()){
+        dir = transform.forward * 10;
+        rb.velocity = new Vector3(0, jumpHeight, 0 );
+        anim.SetBool("isGrounded", false);
+        anim.Play("Jump");
+    }
+
+    public void Evade(Vector3 dir = new Vector3()){
+        if(pManager.isVulnerable){
+            if(dir == Vector3.zero){
+                if(pManager.isLockedOn){
+                    Vector3 tp = pTargeting.currentTarget.transform.position;
+                    tp.y = transform.position.y;
+
+                    transform.LookAt(tp);
+                }
+                anim.Play("StepBack");
+            }else{
+                anim.Play("Roll");
+            }
+            
+            pManager.StartCoroutine(pManager.Invulnerabe());
+        }
+    }
+
+
+    private void BetterJumpPhysics(){
+        if (rb.velocity.y  < 0) 
+            rb.velocity += Vector3.up *  Physics.gravity.y  * (fallMultiplyer - 1) * Time.deltaTime;
+        else if (rb.velocity.y  > 0 /*&& !Input.GetButton("Jump")*/)
+            rb.velocity += Vector3.up * Physics.gravity.y *2 * Time.deltaTime;
+    }
+
+    public void JumpTime(){
+        timeSinceJump = Time.time;
+    }
+
+    public void Land(){
+        float t = Time.time - timeSinceJump;
+
+        if(t > timeTillRoll){
+            //roll
+        }else if(t > timeTillLand){
+            //land
+        }
+    }
+}
